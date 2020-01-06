810e1d577c7e7a8d7e3865811252e499188dbbe5
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-01-06 20:41:27 -0300

    Add title art to medias

--- src/models/Media.ts
+++ src/models/Media.ts
@@ -1,8 +1,9 @@
 export default interface Media {
   id: number;
   title: string;
-  art: string;
-  file: string;
+  art: string; // url
+  titleArt: string; // url
+  file: string; // url
   synopsis: string;
   rating: number;
   duration: string;
